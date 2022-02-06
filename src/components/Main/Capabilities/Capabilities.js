512fc242b9ceee71ea53f0a0094bac1f07264b2c
Peleka <ego.iriska@gmail.com>
2022-02-06 16:32:38 +0300

    fix Capabilities

--- src/components/Main/Capabilities/Capabilities.js
+++ src/components/Main/Capabilities/Capabilities.js
@@ -7,8 +7,24 @@ import icon3 from '../../../images/icon_3.png'
 import {Card} from "./Card/Card";
 import {Title} from "../../../common/components/Title/Title";
 import {Button} from "../../../common/components/Button/Button";
+import {v1} from "uuid";
 
 export const Capabilities = () => {
+    const text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat"
+    const cardsList = [
+        {id: v1(), icon: icon1, title: "PROJECT AND CONSTRUCTION MANAGEMENT", text: text},
+        {id: v1(), icon: icon2, title: "STRUCTURE AUDIT AND MAINTENANCE", text: text},
+        {id: v1(), icon: icon3, title: "FACTORY CONSTRUCTION AND MODELING", text: text},
+    ]
+
+    const cards = cardsList.map(card =>
+        <Card
+            key={card.id}
+            icon={card.icon}
+            title={card.title}
+            text={card.text}
+        />
+    )
 
     return (
         <div className={moduleClasses["sectionAbilities"]}>
@@ -18,27 +34,14 @@ export const Capabilities = () => {
                         modifier={""}
                         title={"Our capabilities"}/>
                     <div className={moduleClasses["sectionAbilities__content"]}>
-                        <p className={moduleClasses["sectionAbilities__text"]}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                        <p className={moduleClasses["sectionAbilities__text"]}>Lorem ipsum dolor sit amet, consectetuer
+                            adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
                             laoreet dolore magna aliquam erat.</p>
                         <Button textButton={"All services"}/>
                     </div>
                 </div>
                 <ul className={moduleClasses["sectionAbilities__list"]}>
-                    <Card
-                        icon={icon1}
-                        title={"PROJECT AND CONSTRUCTION MANAGEMENT"}
-                        text={"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat."}
-                    />
-                   <Card
-                       icon={icon2}
-                       title={"STRUCTURE AUDIT AND MAINTENANCE"}
-                       text={"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat."}
-                   />
-                   <Card
-                       icon={icon3}
-                       title={"FACTORY CONSTRUCTION AND MODELING"}
-                       text={"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat."}
-                   />
+                    {cards}
                 </ul>
             </div>
         </div>
