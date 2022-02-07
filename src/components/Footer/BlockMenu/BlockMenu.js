54128a5d7f8cc99e58807e1be7da7b3b24426223
Peleka <ego.iriska@gmail.com>
2022-02-07 10:32:23 +0300

    fix Footer

--- src/components/Footer/BlockMenu/BlockMenu.js
+++ src/components/Footer/BlockMenu/BlockMenu.js
@@ -3,10 +3,22 @@ import moduleClasses from "./BlockMenu.module.scss";
 
 
 export const BlockMenu = () => {
-    const navLinks = ["Home", "About", "Services", "Projects", "Contacts Us",
-        "FAQ", "Terms", "Careers", "Blog", "Partners", "News"]
+    const navLinks1 = ["Home", "About", "Services", "Projects", "Contacts Us"]
+    const navLinks2 = ["FAQ", "Terms", "Careers"]
+    const navLinks3 = ["Blog", "Partners", "News"]
 
-    const listLinks = navLinks.map((link, index) =>
+
+    const listLinks1 = navLinks1.map((link, index) =>
+        <li className={moduleClasses["menu__link"]} key={index}>
+            <a href={""}>{link}</a>
+        </li>
+    )
+    const listLinks2 = navLinks2.map((link, index) =>
+        <li className={moduleClasses["menu__link"]} key={index}>
+            <a href={""}>{link}</a>
+        </li>
+    )
+    const listLinks3 = navLinks3.map((link, index) =>
         <li className={moduleClasses["menu__link"]} key={index}>
             <a href={""}>{link}</a>
         </li>
@@ -17,7 +29,13 @@ export const BlockMenu = () => {
             <p className={moduleClasses["menu__title"]}>Links</p>
             <div className={moduleClasses["menu__links"]}>
                 <ul className={moduleClasses["menu__list"]}>
-                    {listLinks}
+                    {listLinks1}
+                </ul>
+                <ul className={moduleClasses["menu__list"]}>
+                    {listLinks2}
+                </ul>
+                <ul className={moduleClasses["menu__list"]}>
+                    {listLinks3}
                 </ul>
             </div>
         </div>
